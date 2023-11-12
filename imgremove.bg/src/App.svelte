ed2c6701e23089f45b701b25974eb057d9a8ca36
= <=>
2023-11-12 19:42:19 -0300

    modificando estilos

--- imgremove.bg/src/App.svelte
+++ imgremove.bg/src/App.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
-  import CloudinaryLogo from './assets/CloudinaryLogo.svelte';
-  import StepUpload from './StepUpload.svelte';
-  import StepEdit from './StepEdit.svelte';
-  import { imageStatus } from './store';
-  import { ImageStatus } from './types.d';
+  import CloudinaryLogo from "./assets/CloudinaryLogo.svelte";
+  import StepUpload from "./StepUpload.svelte";
+  import StepEdit from "./StepEdit.svelte";
+  import { imageStatus } from "./store";
+  import { ImageStatus } from "./types.d";
 </script>
 
-
-<div class="max-m-xl grid-cols-1 m-uto place-content-center w-full h-screen p-4">
-  <header class="justify justify-center py-10 bg-gray-200">
-    <h1 class="text-3xl font-bold text-orange-600 text-center tracking-tighter">remove<span class="text-blue-500">bg</span></h1>
+<div
+  class="max-m-xl grid-cols-1 m-auto place-content-center w-full h-screen p-0"
+>
+  <header class="justify justify-center py-10 bg-black">
+    <h1 class="text-3xl font-bold text-orange-600 text-center tracking-tighter">
+      remove<span class="text-blue-500">bg</span>
+    </h1>
   </header>
   <main class="w-full block">
     {#if $imageStatus === ImageStatus.READY || $imageStatus === ImageStatus.UPLOADING}
@@ -18,8 +21,12 @@
       <StepEdit />
     {/if}
   </main>
-  
-  <footer class="flex justify-center items-center gap-x-2 font-semibold pt-10">
-    Hecho con <a href="https://cloudinary" target="_blank" rel="noreferrer"><CloudinaryLogo/></a>
+
+  <footer
+    class="flex justify-center bg-black text-white items-center gap-x-2 font-semibold p-6"
+  >
+    Hecho con <a href="https://cloudinary" target="_blank" rel="noreferrer"
+      ><CloudinaryLogo /></a
+    > por Jonatan Villalva
   </footer>
 </div>
--- imgremove.bg/src/StepUpload.svelte
+++ imgremove.bg/src/StepUpload.svelte
@@ -1,69 +1,68 @@
 <script lang="ts">
-    import { Cloudinary } from '@cloudinary/url-gen';
-    import { backgroundRemoval } from '@cloudinary/url-gen/actions/effect';
-    import { ImageStatus } from './types.d'; 
-    import { imageStatus, modifiedImage, originalImage } from './store';
-    import Dropzone from 'dropzone';
-    import 'dropzone/dist/dropzone.css';
-    import { onMount } from 'svelte';
+  import { Cloudinary } from "@cloudinary/url-gen";
+  import { backgroundRemoval } from "@cloudinary/url-gen/actions/effect";
+  import { ImageStatus } from "./types.d";
+  import { imageStatus, modifiedImage, originalImage } from "./store";
+  import Dropzone from "dropzone";
+  import "dropzone/dist/dropzone.css";
+  import { onMount } from "svelte";
 
-    const cloudinary = new Cloudinary({
-        cloud:{
-            cloudName: 'ddeun9v2x'
-        },
-        url:{
-            secure: true
-        }
-    })
+  const cloudinary = new Cloudinary({
+    cloud: {
+      cloudName: "ddeun9v2x",
+    },
+    url: {
+      secure: true,
+    },
+  });
 
-    onMount(()=>{
-        const dropzone = new Dropzone("#dropzone",{
-            uploadMultiple: false,
-            acceptedFiles: ".jpg , .png , .webp , .jpeg",
-            maxFiles: 1
-        })
-        dropzone.on('sending',(file,xhr,formData)=>{
-            imageStatus.set(ImageStatus.UPLOADING)
-            formData.append('upload_preset','dd14odww');
-            formData.append('timestamp',Date.now()/1000);
-            formData.append('api_key',161377236574727);
-        })
-        dropzone.on('success',(file,response)=>{
-            const {
-                public_id : publicId ,
-                secure_url: url
-            } = response
+  onMount(() => {
+    const dropzone = new Dropzone("#dropzone", {
+      uploadMultiple: false,
+      acceptedFiles: ".jpg , .png , .webp , .jpeg",
+      maxFiles: 1,
+    });
+    dropzone.on("sending", (file, xhr, formData) => {
+      imageStatus.set(ImageStatus.UPLOADING);
+      formData.append("upload_preset", "dd14odww");
+      formData.append("timestamp", Date.now() / 1000);
+      formData.append("api_key", 161377236574727);
+    });
+    dropzone.on("success", (file, response) => {
+      const { public_id: publicId, secure_url: url } = response;
 
-            const imageWithoutBackground = cloudinary
-            .image(publicId)
-            .effect(backgroundRemoval())
+      const imageWithoutBackground = cloudinary
+        .image(publicId)
+        .effect(backgroundRemoval());
 
-            imageStatus.set(ImageStatus.DONE)
-            modifiedImage.set(imageWithoutBackground.toURL())
-            originalImage.set(url)
+      imageStatus.set(ImageStatus.DONE);
+      modifiedImage.set(imageWithoutBackground.toURL());
+      originalImage.set(url);
 
-            //crear imagen con fondo transparente
-            // y guardar en el backgroundImage
-        })
-        dropzone.on('error',(file,response)=>{
-            console.log("ha ido mal");
-            console.log(response);
-        })
-    })
+      //crear imagen con fondo transparente
+      // y guardar en el backgroundImage
+    });
+    dropzone.on("error", (file, response) => {
+      console.log("ha ido mal");
+      console.log(response);
+    });
+  });
 </script>
 
-<form id="dropzone" 
-class="shadow-2xl border-dashed bg-orange-100 border-2 border-gray-400 redounded-lg aspect-video w-full flex justify-center items-center flex-col"
-action="https://api.cloudinary.com/v1_1/ddeun9v2x/image/upload"
+<form
+  id="dropzone"
+  class="shadow-2xl border-dashed bg-gray-400 border-2 border-orange-600 redounded-lg aspect-video w-full flex justify-center items-center flex-col"
+  action="https://api.cloudinary.com/v1_1/ddeun9v2x/image/upload"
 >
-    {#if $imageStatus === ImageStatus.READY}
-    <button class="pointer-events-none bg-blue-600 rounded-xl 
-    text-bold text-white text-xl px-6 py-4">
-        Upload files
+  {#if $imageStatus === ImageStatus.READY}
+    <button
+      class="pointer-events-none bg-blue-600 rounded-xl
+    text-bold text-white text-xl px-6 py-4"
+    >
+      Upload files
     </button>
-    <strong class="text-lg mt-4 text-gray-500">Or drop a file</strong>
-
-    {:else if $imageStatus === ImageStatus.UPLOADING}
-        <strong class="text-lg mt-4 text-gray-500">Uploading files</strong>
-    {/if}
+    <strong class="text-lg mt-4 text-orange-600">Or drop a file</strong>
+  {:else if $imageStatus === ImageStatus.UPLOADING}
+    <strong class="text-lg mt-4 text-orange-600">Uploading files</strong>
+  {/if}
 </form>
